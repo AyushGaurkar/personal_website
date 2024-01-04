844d81e6e34a665a9f80e79ae689582a118a17d6
AyushGaurkar <ayushgaurkar40@gmail.com>
2024-01-04 16:21:20 +0530

    Initial commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,84 @@
+import bannerImage from "../assets/bi1.jpg";
+import bannerBackground from "../assets/banner_wallpaper.svg";
+import Typed from "typed.js";
+import { useRef } from "react";
+import { useEffect } from "react";
+const Banner = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Fullstack Developer", "Frontend Developer", "Backend Developer"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 100,
+      typeSpeed: 50,
+      backSpeed: 10,
+      backDelay: 100,
+      loop: true,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${bannerBackground})`,
+        backgroundSize: "cover",
+      }}
+      className="main-container flex items-center"
+    >
+      {/* first dabba */}
+      <div className="w-full flex items-center justify-center text-white ">
+        {/* text */}
+        <div className="w-2/3 space-y-2  ms-10">
+          <h3 className="text-3xl font-semibold">Hi, I am</h3>
+          <h1 className="text-5xl font-bold">Ayush Gaurkar</h1>
+          <h2 className="text-3xl ">
+            I am <span className="font-bold underline" ref={el}></span>
+          </h2>
+          <p className="">
+          a Java Fullstack Developer passionate about crafting seamless digital experiences. With expertise in front-end technologies like HTML, CSS, and JavaScript, coupled with a strong back-end foundation in Java, I specialize in building robust and user-friendly applications. Check out my portfolio to explore projects showcasing my skills in Spring Boot, React.js, and more. Let's connect and create something extraordinary together! 🚀
+          </p>
+
+          <div className="icons-container flex space-x-5">
+            <a className=" hover:bg-orange-600 border cursor-pointer  px-3 py-4 w-16 h-16  rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands text-4xl  fa-facebook"></i>
+            </a>
+
+            <a href="https://www.instagram.com/ayush.gaurkar10/"  className=" hover:bg-orange-600   border cursor-pointer  px-3 py-4 w-16 h-16  rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands text-4xl  fa-instagram"></i>
+            </a>
+            
+            <a href="https://www.linkedin.com/in/ayush-gaurkar-8b19121aa/" className=" hover:bg-orange-600  border  cursor-pointer px-3 py-4 w-16 h-16  rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands text-4xl  fa-linkedin-in" ></i>
+            </a>
+
+            <a href="https://twitter.com/ayush_gaurkar" className=" hover:bg-orange-600  border  cursor-pointer px-3 py-4 w-16 h-16  rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands fa-x-twitter"></i>
+            </a>
+          </div>
+          <br />
+          <a
+            className=" text-2xl     px-3  py-2 bg-orange-500  rounded-full shadow-lg"
+            href="/contact"
+          >
+            Contact Me
+          </a>
+        </div>
+      </div>
+
+      {/* second  dabba */}
+
+      <div className=" w-full flex justify-center">
+        {/* image */}
+        <img className="rounded-full my-2 shadow-lg w-fit" src={bannerImage} />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
